02a96ca29ce7c60981c8b17c6d2242763d6aa91d
Zixi Yan <yanzix@shernet.sheridancollege.ca>
2026-01-21 09:48:05 -0500

    Changed the clor of a sprite

---
+++ Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpriteChanger : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Color col;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //pickARandomColour();
+        //spriteRenderer.color = col;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        //{
+        //    //pickARandomColour();
+        //}
+
+        //get the mouse position
+        Vector2 mousePos = Camera.main.ScreenToViewportPoint(Mouse.current.position.ReadValue());
+        //is it over sprite
+        if (spriteRenderer.bounds.Contains(mousePos) == true)
+        {
+            //Y: use the col variable
+            spriteRenderer.color = col;
+        }
+        else
+        {
+            //N: set color to white
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    void pickARandomColour()
+    {
+        spriteRenderer.color = Random.ColorHSV();
+    }
+}
